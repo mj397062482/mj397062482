a2f811449d94527f824a284c396809515455546f
mj397062482 <397062482@qq.com>
2021-07-21 15:22:56 +0800

    123

--- 1.cpp
+++ 1.cpp
@@ -1,49 +1,15 @@
-#include <fstream>
 #include <iostream>
 using namespace std;
  
 int main ()
 {
-    
-   char data[100];
+   double* pvalue  = NULL; // 初始化为 null 的指针
+   pvalue  = new double;   // 为变量请求内存
  
-   // 以写模式打开文件
-   ofstream outfile;
-   outfile.open("afile.dat");
+   *pvalue = 29494.99;     // 在分配的地址存储值
+   cout << "Value of pvalue : " << *pvalue << endl;
  
-   cout << "Writing to the file" << endl;
-   cout << "Enter your name: "; 
-   cin.getline(data, 100);
- 
-   // 向文件写入用户输入的数据
-   outfile << data << endl;
- 
-   cout << "Enter your age: "; 
-   cin >> data;
-   cin.ignore();
-   
-   // 再次向文件写入用户输入的数据
-   outfile << data << endl;
- 
-   // 关闭打开的文件
-   outfile.close();
- 
-   // 以读模式打开文件
-   ifstream infile; 
-   infile.open("afile.dat"); 
- 
-   cout << "Reading from the file" << endl; 
-   infile >> data; 
- 
-   // 在屏幕上写入数据
-   cout << data << endl;
-   
-   // 再次从文件读取数据，并显示它
-   infile >> data; 
-   cout << data << endl; 
- 
-   // 关闭打开的文件
-   infile.close();
+   delete pvalue;         // 释放内存
  
    return 0;
 }
