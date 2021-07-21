4f2d409bd413ba94a63a9ebe6012207e190d1dc1
mj397062482 <397062482@qq.com>
2021-07-21 15:24:57 +0800

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -6,7 +6,7 @@ int main ()
    double* pvalue  = NULL; // 初始化为 null 的指针
    pvalue  = new double;   // 为变量请求内存
  
-   *pvalue = 29494.99;     // 在分配的地址存储值
+   *pvalue = 29495.99;     // 在分配的地址存储值
    cout << "Value of pvalue : " << *pvalue << endl;
  
    delete pvalue;         // 释放内存
